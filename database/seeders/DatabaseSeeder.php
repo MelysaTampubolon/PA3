da23f481602a4e8aa86127316ee1e03087afd931
ifetayo14 <alvinsimbolon6@gmail.com>
2021-06-08 11:39:32 +0700

    hostCommit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
-        $this->call(SupplierSeeder::class);
-        $this->call(TokoOnlineSeeder::class);
-        $this->call(ConfigSeeder::class);
-//        $this->call(SumberDataSeeder::class);
+//        $this->call(UserSeeder::class);
+//        $this->call(SupplierSeeder::class);
+//        $this->call(TokoOnlineSeeder::class);
+//        $this->call(ConfigSeeder::class);
+        $this->call(SumberDataSeeder::class);
 //        $this->call(ProductController::class);
         // \App\Models\User::factory(10)->create();
     }
