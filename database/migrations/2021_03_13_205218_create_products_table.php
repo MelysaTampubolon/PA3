476d1e6a680c161c33ed4009c1e9d881fbea5589
ifetayo14 <alvinsimbolon6@gmail.com>
2021-06-08 09:11:30 +0700

    hostCommit

--- database/migrations/2021_03_13_205218_create_products_table.php
+++ database/migrations/2021_03_13_205218_create_products_table.php
@@ -17,12 +17,20 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->unsignedBigInteger('riwayat_id');
             $table->foreign('riwayat_id')->references('id')->on('riwayat_fetch_data');
+            $table->string('kategoriToped');
+            $table->string('kategoriShopee');
+            $table->string('kategoriBukaLapak');
+            $table->string('kode_produk');
             $table->string('url_produk');
             $table->string('nama_produk');
             $table->integer('stok');
             $table->double('harga');
-            $table->longText('gambar');
-            $table->string('kode_produk');
+            $table->string('gambar');
+            $table->string('deskripsi');
+            $table->integer('berat');
+            $table->integer('waktuPreorder');
+            $table->string('asuransi');
+            $table->string('dummyColumn');
             $table->timestamps();
         });
     }
